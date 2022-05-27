4bc14846877a1333bd51d405959167f3730057a0
neko113 <alstn113@gmail.com>
2022-05-27 15:26:09 +0900

    feat : config properties 분리

---
+++ cat/src/config/properties.js
@@ -0,0 +1,4 @@
+export const PROPERTIES = {
+  API_END_POINT:
+    "https://zl3m4qq0l9.execute-api.ap-northeast-2.amazonaws.com/dev",
+};
--- cat/src/lib/api.js
+++ cat/src/lib/api.js
@@ -1,9 +1,10 @@
-const API_END_POINT =
-  "https://zl3m4qq0l9.execute-api.ap-northeast-2.amazonaws.com/dev";
+import { PROPERTIES } from "../config/properties.js";
 
 const request = async (nodeId) => {
   try {
-    const res = await fetch(`${API_END_POINT}/${nodeId ? nodeId : ""}`);
+    const res = await fetch(
+      `${PROPERTIES.API_END_POINT}/${nodeId ? nodeId : ""}`
+    );
     if (!res.ok) throw new Error("서버의 상태가 이상합니다!");
     return await res.json();
   } catch (err) {
